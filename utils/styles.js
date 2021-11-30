35f4e1f61ad3e371239f54c2ca36a23bb338f92e
Ibrahim Mohamed <ibrahimm.mourad97@gmail.com>
2021-11-30 17:36:29 +0200

    add Place Order Page and Save Order to DB

--- utils/styles.js
+++ utils/styles.js
@@ -18,7 +18,10 @@ const useStyles = makeStyles({
   },
   footer: { marginTop: 10, textAlign: 'center' },
   //
-  section: { marginTop: 10, marginBottom: 10 },
+  section: {
+    marginTop: 10,
+    marginBottom: 10,
+  },
   form: {
     maxWidth: 800,
     margin: '0 auto',
