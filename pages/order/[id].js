1a090f7c974f441a12fdc640909488c376c51770
Ibrahim Mohamed <ibrahimm.mourad97@gmail.com>
2021-12-01 12:17:22 +0200

    Create Order Details Page

---
+++ pages/order/[id].js
@@ -0,0 +1,260 @@
+import React, { useContext, useEffect, useReducer } from 'react';
+import NextLink from 'next/link';
+import dynamic from 'next/dynamic';
+import Image from 'next/image';
+import { useSnackbar } from 'notistack';
+import { useRouter } from 'next/router';
+import axios from 'axios';
+import {
+  Card,
+  CircularProgress,
+  Grid,
+  Link,
+  List,
+  ListItem,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@material-ui/core';
+
+import Layout from '../../components/Layout';
+import CheckoutWizard from '../../components/CheckoutWizard';
+import { Store } from '../../utils/Store';
+import useStyles from '../../utils/styles';
+import { getError } from '../../utils/error';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true, error: '' };
+    case 'FETCH_SUCCESS':
+      return { ...state, loading: false, order: action.payload, error: '' };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+const Order = ({ params }) => {
+  const orderId = params.id;
+  const classes = useStyles();
+  const router = useRouter();
+  const { state } = useContext(Store);
+  const { userInfo } = state;
+
+  const [{ loading, error, order }, dispatch] = useReducer(reducer, {
+    loading: true,
+    order: {},
+    err: '',
+  });
+
+  const {
+    shippingAddress,
+    paymentMethod,
+    orderItems,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+    isPaid,
+    paidAt,
+    isDelivered,
+    deliveredAt,
+  } = order;
+
+  useEffect(() => {
+    if (!userInfo) {
+      router.push('/login');
+    }
+    const fetchOrder = async () => {
+      try {
+        dispatch({ type: 'FETCH_REQUEST' });
+        const { data } = await axios.get(`/api/orders/${orderId}`, {
+          headers: { authorization: `Bearer ${userInfo.token}` },
+        });
+        dispatch({ type: 'FETCH_SUCCESS', payload: data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
+      }
+    };
+    if (!order._id || (order._id && order._id !== orderId)) {
+      fetchOrder();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [order]);
+
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+
+  return (
+    <Layout title={`Order ${orderId}`}>
+      <CheckoutWizard activeStep={3} />
+      <Typography component="h1" variant="h1">
+        Order {orderId}
+      </Typography>
+      {loading ? (
+        <CircularProgress />
+      ) : error ? (
+        <Typography className={classes.error}>{error}</Typography>
+      ) : (
+        <Grid container spacing={1}>
+          <Grid item md={9} xs={12}>
+            <Card className={classes.section}>
+              <List>
+                <ListItem>
+                  <Typography component="h2" variant="h2">
+                    Shipping Address
+                  </Typography>
+                </ListItem>
+                <ListItem>
+                  {shippingAddress.fullName}, {shippingAddress.address},{' '}
+                  {shippingAddress.city}, {shippingAddress.postalCode},{' '}
+                  {shippingAddress.country}
+                </ListItem>
+                <ListItem>
+                  Status:
+                  {isDelivered ? `delivered at ${deliveredAt}` : 'not deliverd'}
+                </ListItem>
+              </List>
+            </Card>
+            <Card className={classes.section}>
+              <List>
+                <ListItem>
+                  <Typography component="h2" variant="h2">
+                    Payment Method
+                  </Typography>
+                </ListItem>
+                <ListItem>{paymentMethod}</ListItem>
+                <ListItem>
+                  Status: {isPaid ? ` paid at ${paidAt}` : ' not paid'}
+                </ListItem>
+              </List>
+            </Card>
+
+            <Card className={classes.section}>
+              <List>
+                <ListItem>
+                  <Typography component="h2" variant="h2">
+                    Order Items
+                  </Typography>
+                </ListItem>
+                <ListItem>
+                  <TableContainer>
+                    <Table>
+                      <TableHead>
+                        <TableRow>
+                          <TableCell>Image</TableCell>
+                          <TableCell>Name</TableCell>
+                          <TableCell align="right">Quantity</TableCell>
+                          <TableCell align="right">Price</TableCell>
+                        </TableRow>
+                      </TableHead>
+                      <TableBody>
+                        {orderItems.map((item) => (
+                          <TableRow key={item._id}>
+                            <TableCell>
+                              <NextLink href={`/product/${item.slug}`} passHref>
+                                <Link>
+                                  <Image
+                                    src={item.image}
+                                    alt={item.name}
+                                    width="50"
+                                    height="50"
+                                  />
+                                </Link>
+                              </NextLink>
+                            </TableCell>
+
+                            <TableCell>
+                              <NextLink href={`/product/${item.slug}`} passHref>
+                                <Link>
+                                  <Typography> {item.name}</Typography>
+                                </Link>
+                              </NextLink>
+                            </TableCell>
+
+                            <TableCell align="right">
+                              <Typography>{item.quantity}</Typography>
+                            </TableCell>
+                            <TableCell align="right">
+                              <Typography>${item.price}</Typography>
+                            </TableCell>
+                          </TableRow>
+                        ))}
+                      </TableBody>
+                    </Table>
+                  </TableContainer>
+                </ListItem>
+              </List>
+            </Card>
+          </Grid>
+          <Grid item md={3} xs={12}>
+            <Card className={classes.section}>
+              <List>
+                <ListItem>
+                  <Typography variant="h2">Order Summary</Typography>
+                </ListItem>
+
+                <ListItem>
+                  <Grid container>
+                    <Grid item xs={6}>
+                      <Typography>Items:</Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                      <Typography align="right">${itemsPrice}</Typography>
+                    </Grid>
+                  </Grid>
+                </ListItem>
+                {/* Tax */}
+                <ListItem>
+                  <Grid container>
+                    <Grid item xs={6}>
+                      <Typography>Tax:</Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                      <Typography align="right">${taxPrice}</Typography>
+                    </Grid>
+                  </Grid>
+                </ListItem>
+                {/* Shipping */}
+                <ListItem>
+                  <Grid container>
+                    <Grid item xs={6}>
+                      <Typography>Shipping:</Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                      <Typography align="right">${shippingPrice}</Typography>
+                    </Grid>
+                  </Grid>
+                </ListItem>
+                {/* Total */}
+                <ListItem>
+                  <Grid container>
+                    <Grid item xs={6}>
+                      <Typography>
+                        <strong>Total:</strong>
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                      <Typography align="right">
+                        <strong>${totalPrice}</strong>
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                </ListItem>
+              </List>
+            </Card>
+          </Grid>
+        </Grid>
+      )}
+    </Layout>
+  );
+};
+export const getServerSideProps = async ({ params }) => {
+  return { props: { params } };
+};
+export default dynamic(() => Promise.resolve(Order), { ssr: false });
