b23d93c1a6a17de08f6786cc6e66633c7d99969c
Ibrahim Mourad <ibrahimm.mourad97@gmail.com>
2021-12-20 17:07:31 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   reactStrictMode: true,
-  env: {
-    PAYPAL_CLIENT_ID: // your paypal id here
-  },
+ // env: {
+ //  PAYPAL_CLIENT_ID:  your paypal id here
+ // },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
