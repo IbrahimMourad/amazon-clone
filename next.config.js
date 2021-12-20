6ac62dfbba80e62f963e69810d56227fc49a195e
Ibrahim Mohamed <ibrahimm.mourad97@gmail.com>
2021-12-20 16:32:50 +0200

    fix linting

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   reactStrictMode: true,
+  ignoreDuringBuilds: true,
   env: {
     PAYPAL_CLIENT_ID:
       'AfIpFjK7IsxubLrrvPnfpq0zUleGzsNWSJsQ-CSTIyviAzaa7_EOwKIxVv7NOVt40rIgSG4IMd3rEZK8',
