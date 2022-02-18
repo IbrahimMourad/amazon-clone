66fc883611c098ddd13dc10826089ec68400c086
Ibrahim Mohamed <ibrahimm.mourad97@gmail.com>
2022-02-19 00:40:41 +0200

    fix cloudinary config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   // reactStrictMode: true,
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
   env: {
     PAYPAL_CLIENT_ID:
       'AfIpFjK7IsxubLrrvPnfpq0zUleGzsNWSJsQ-CSTIyviAzaa7_EOwKIxVv7NOVt40rIgSG4IMd3rEZK8',
